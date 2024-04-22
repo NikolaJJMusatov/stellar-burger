3637f4a9688a8db08fe7c2c9e1810ebc787b6e21
Nikolai Musatov <musatovnikolajj@rambler.ru>
2024-04-22 18:00:50 +0400

    установлены/настроены пакеты тестов

---
+++ cypress/e2e/constructorPage/constructor.cy.tsx
@@ -0,0 +1,9 @@
+
+
+describe('проверяем доступность приложения', function() {
+  it('сервис должен быть доступен по адресу localhost:4000', function() {
+      cy.visit('http://localhost:4000'); 
+  });
+});
+
+
